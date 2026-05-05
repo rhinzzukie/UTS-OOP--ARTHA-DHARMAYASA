1e847504f80a18b1aa6df89aefbd4cc3c0bf6d51
rhinzz <dharmayasaartha@gmail.com>
2026-05-05 10:50:45 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("===========================");
+        System.out.println("SELAMAT DATANG DI RESTORANT");
+        System.out.println("SILAHKAN PILIH MENU (NOMOR) ANDA !");
+        System.out.println("===========================");
+
+    }
+    
+}
